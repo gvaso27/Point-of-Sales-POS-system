f0d3acceadb4be6489ddd410ea64cd4fcf15b93f
zuka <zukashakulashvili2002@gmail.com>
2025-02-22 20:44:13 +0400

    small fix

--- Final/app/infrastructure/fastapi/product.py
+++ Final/app/infrastructure/fastapi/product.py
@@ -1,24 +1,27 @@
 from __future__ import annotations
 
-from typing import Any
+from typing import Any, no_type_check
 from uuid import UUID
 
 from fastapi import APIRouter
 from starlette.responses import JSONResponse
 
+from app.core.product import ProductService
 from app.infrastructure.fastapi.dependables import ProductRepositoryDependable
 
-product_api = APIRouter(tags=["Products"])
-
+product_api: APIRouter = APIRouter()
 
 @product_api.get(
-    "/products/{product_id}", status_code=200, response_model=dict[str, Any]
+    "/products/{product_id}",
+    status_code=200,
+    response_model=dict[str, Any]
 )
+@no_type_check
 def read_product(
     product_id: UUID, products: ProductRepositoryDependable
 ) -> dict[str, Any] | JSONResponse:
     try:
-        return {"product": products.read(product_id)}
+        return {"product": ProductService(products).read(product_id)}
     except Exception as e:
         return JSONResponse(
             status_code=404,
