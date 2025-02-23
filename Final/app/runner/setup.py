2702b81984104bb62e462bd4bbb8727401572a56
zuka <zukashakulashvili2002@gmail.com>
2025-02-23 16:49:57 +0400

    add product api tests

--- Final/app/runner/setup.py
+++ Final/app/runner/setup.py
@@ -1,10 +1,11 @@
 from fastapi import FastAPI
 
 from app.infrastructure.fastapi.product import product_api
+from app.infrastructure.sqlite.producs_in_memory_db import InMemoryProductDb
 from app.infrastructure.sqlite.product_db import ProductDb
 
 
-def init_app() -> FastAPI:
+def init_app(db_type: str = "sqlite") -> FastAPI:
     app = FastAPI()
 
     # TODO:
@@ -12,6 +13,9 @@ def init_app() -> FastAPI:
     # receipt
     app.include_router(product_api)
 
-    app.state.product = ProductDb()
+    if db_type == "sqlite":
+        app.state.product = ProductDb()
+    else:
+        app.state.product = InMemoryProductDb()
 
     return app
