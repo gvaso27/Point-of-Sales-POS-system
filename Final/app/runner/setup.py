941ab50258e1c661ba513122684c85182c3e6d6e
saba <sabagvasalia26@gmail.com>
2025-02-23 16:54:03 +0400

    fixed ruff

--- Final/app/runner/setup.py
+++ Final/app/runner/setup.py
@@ -4,7 +4,6 @@ from app.infrastructure.fastapi.campaign import campaign_api
 from app.infrastructure.fastapi.product import product_api
 from app.infrastructure.sqlite.campaign_db import CampaignDb
 from app.infrastructure.sqlite.producs_in_memory_db import InMemoryProductDb
-
 from app.infrastructure.sqlite.product_db import ProductDb
 
 
@@ -22,6 +21,5 @@ def init_app(db_type: str = "sqlite") -> FastAPI:
         app.state.product = ProductDb()
     else:
         app.state.product = InMemoryProductDb()
-
 
     return app
